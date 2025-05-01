33706a3beba206cf8facf8cf5209c361d54fbbbe
Zenn771 <haritsnaufal48@gmail.com>
2025-05-01 12:04:38 +0000

    Refactor: Remove duplicate career timeline section
    Removed the career timeline section from the Career page and updated the navigation to point to the timeline on the main page.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Career from "./pages/Career";
 
 const queryClient = new QueryClient();
 
@@ -18,7 +17,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/career" element={<Career />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
