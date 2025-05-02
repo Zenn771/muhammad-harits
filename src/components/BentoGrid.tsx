53bbcca4f71abb74e8a230e028b9c88dec878f8e
Zenn771 <haritsnaufal48@gmail.com>
2025-05-02 13:11:19 +0000

    Fix: Improve Bento grid layout
    Refactor the Bento grid layout to ensure a more symmetrical and visually appealing arrangement of cards.

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -7,9 +7,9 @@ import MacBookFrame from './MacBookFrame';
 const BentoBox = () => {
   return (
     <div className="mx-auto max-w-6xl px-4">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
-        {/* MacBook Frame - Placed as first item */}
-        <div className="col-span-1 md:col-span-1 lg:col-span-1 row-span-2">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-6">
+        {/* First Row */}
+        <div className="col-span-1 md:col-span-1 row-span-2">
           <MacBookFrame 
             imageSrc="/placeholder.svg" 
             className="h-full"
@@ -18,18 +18,86 @@ const BentoBox = () => {
           />
         </div>
         
-        {/* Regular Bento Cards */}
-        {bentoCardsData.map((card, index) => (
+        <div className="col-span-1 md:col-span-2">
           <BentoCard
-            key={index}
-            title={card.title}
-            description={card.description}
-            icon={<card.icon className={`h-5 w-5 text-${card.gradient.split('-')[1]}-300`} />}
-            gradient={card.gradient}
-            delay={card.delay}
-            sizeClasses={card.sizeClasses}
+            title={bentoCardsData[0].title}
+            description={bentoCardsData[0].description}
+            icon={<bentoCardsData[0].icon className={`h-5 w-5 text-${bentoCardsData[0].gradient.split('-')[1]}-300`} />}
+            gradient={bentoCardsData[0].gradient}
+            delay={bentoCardsData[0].delay}
+            sizeClasses="col-span-1 md:col-span-2 row-span-1"
           />
-        ))}
+        </div>
+        
+        {/* Second Row */}
+        <div className="col-span-1 md:col-span-1">
+          <BentoCard
+            title={bentoCardsData[1].title}
+            description={bentoCardsData[1].description}
+            icon={<bentoCardsData[1].icon className={`h-5 w-5 text-${bentoCardsData[1].gradient.split('-')[1]}-300`} />}
+            gradient={bentoCardsData[1].gradient}
+            delay={bentoCardsData[1].delay}
+            sizeClasses="col-span-1 row-span-1"
+          />
+        </div>
+        
+        <div className="col-span-1 md:col-span-1">
+          <BentoCard
+            title={bentoCardsData[2].title}
+            description={bentoCardsData[2].description}
+            icon={<bentoCardsData[2].icon className={`h-5 w-5 text-${bentoCardsData[2].gradient.split('-')[1]}-300`} />}
+            gradient={bentoCardsData[2].gradient}
+            delay={bentoCardsData[2].delay}
+            sizeClasses="col-span-1 row-span-1"
+          />
+        </div>
+        
+        {/* Third Row */}
+        <div className="col-span-1 md:col-span-1">
+          <BentoCard
+            title={bentoCardsData[3].title}
+            description={bentoCardsData[3].description}
+            icon={<bentoCardsData[3].icon className={`h-5 w-5 text-${bentoCardsData[3].gradient.split('-')[1]}-300`} />}
+            gradient={bentoCardsData[3].gradient}
+            delay={bentoCardsData[3].delay}
+            sizeClasses="col-span-1 row-span-2"
+          />
+        </div>
+        
+        <div className="col-span-1 md:col-span-2">
+          <BentoCard
+            title={bentoCardsData[4].title}
+            description={bentoCardsData[4].description}
+            icon={<bentoCardsData[4].icon className={`h-5 w-5 text-${bentoCardsData[4].gradient.split('-')[1]}-300`} />}
+            gradient={bentoCardsData[4].gradient}
+            delay={bentoCardsData[4].delay}
+            sizeClasses="col-span-1 md:col-span-2 row-span-1"
+          />
+        </div>
+        
+        {/* Fourth Row */}
+        <div className="col-span-1 md:col-span-1">
+          <BentoCard
+            title={bentoCardsData[5].title}
+            description={bentoCardsData[5].description}
+            icon={<bentoCardsData[5].icon className={`h-5 w-5 text-${bentoCardsData[5].gradient.split('-')[1]}-300`} />}
+            gradient={bentoCardsData[5].gradient}
+            delay={bentoCardsData[5].delay}
+            sizeClasses="col-span-1 row-span-1"
+          />
+        </div>
+        
+        {/* Fifth Row */}
+        <div className="col-span-1 md:col-span-3">
+          <BentoCard
+            title={bentoCardsData[6].title}
+            description={bentoCardsData[6].description}
+            icon={<bentoCardsData[6].icon className={`h-5 w-5 text-${bentoCardsData[6].gradient.split('-')[1]}-300`} />}
+            gradient={bentoCardsData[6].gradient}
+            delay={bentoCardsData[6].delay}
+            sizeClasses="col-span-1 md:col-span-3 row-span-1"
+          />
+        </div>
       </div>
     </div>
   );
