07f6bbf208ec886c66fdb1977fa717f451601125
Zenn771 <haritsnaufal48@gmail.com>
2025-05-02 13:12:06 +0000

    Fix: BentoGrid syntax errors
    Fixes syntax errors in BentoGrid.tsx related to the way the icon is rendered, specifically the use of template literals within the JSX.

--- src/components/BentoGrid.tsx
+++ src/components/BentoGrid.tsx
@@ -22,7 +22,9 @@ const BentoBox = () => {
           <BentoCard
             title={bentoCardsData[0].title}
             description={bentoCardsData[0].description}
-            icon={<bentoCardsData[0].icon className={`h-5 w-5 text-${bentoCardsData[0].gradient.split('-')[1]}-300`} />}
+            icon={React.createElement(bentoCardsData[0].icon, { 
+              className: `h-5 w-5 text-${bentoCardsData[0].gradient.split('-')[1]}-300` 
+            })}
             gradient={bentoCardsData[0].gradient}
             delay={bentoCardsData[0].delay}
             sizeClasses="col-span-1 md:col-span-2 row-span-1"
@@ -34,7 +36,9 @@ const BentoBox = () => {
           <BentoCard
             title={bentoCardsData[1].title}
             description={bentoCardsData[1].description}
-            icon={<bentoCardsData[1].icon className={`h-5 w-5 text-${bentoCardsData[1].gradient.split('-')[1]}-300`} />}
+            icon={React.createElement(bentoCardsData[1].icon, { 
+              className: `h-5 w-5 text-${bentoCardsData[1].gradient.split('-')[1]}-300` 
+            })}
             gradient={bentoCardsData[1].gradient}
             delay={bentoCardsData[1].delay}
             sizeClasses="col-span-1 row-span-1"
@@ -45,7 +49,9 @@ const BentoBox = () => {
           <BentoCard
             title={bentoCardsData[2].title}
             description={bentoCardsData[2].description}
-            icon={<bentoCardsData[2].icon className={`h-5 w-5 text-${bentoCardsData[2].gradient.split('-')[1]}-300`} />}
+            icon={React.createElement(bentoCardsData[2].icon, { 
+              className: `h-5 w-5 text-${bentoCardsData[2].gradient.split('-')[1]}-300` 
+            })}
             gradient={bentoCardsData[2].gradient}
             delay={bentoCardsData[2].delay}
             sizeClasses="col-span-1 row-span-1"
@@ -57,7 +63,9 @@ const BentoBox = () => {
           <BentoCard
             title={bentoCardsData[3].title}
             description={bentoCardsData[3].description}
-            icon={<bentoCardsData[3].icon className={`h-5 w-5 text-${bentoCardsData[3].gradient.split('-')[1]}-300`} />}
+            icon={React.createElement(bentoCardsData[3].icon, { 
+              className: `h-5 w-5 text-${bentoCardsData[3].gradient.split('-')[1]}-300` 
+            })}
             gradient={bentoCardsData[3].gradient}
             delay={bentoCardsData[3].delay}
             sizeClasses="col-span-1 row-span-2"
@@ -68,7 +76,9 @@ const BentoBox = () => {
           <BentoCard
             title={bentoCardsData[4].title}
             description={bentoCardsData[4].description}
-            icon={<bentoCardsData[4].icon className={`h-5 w-5 text-${bentoCardsData[4].gradient.split('-')[1]}-300`} />}
+            icon={React.createElement(bentoCardsData[4].icon, { 
+              className: `h-5 w-5 text-${bentoCardsData[4].gradient.split('-')[1]}-300` 
+            })}
             gradient={bentoCardsData[4].gradient}
             delay={bentoCardsData[4].delay}
             sizeClasses="col-span-1 md:col-span-2 row-span-1"
@@ -80,7 +90,9 @@ const BentoBox = () => {
           <BentoCard
             title={bentoCardsData[5].title}
             description={bentoCardsData[5].description}
-            icon={<bentoCardsData[5].icon className={`h-5 w-5 text-${bentoCardsData[5].gradient.split('-')[1]}-300`} />}
+            icon={React.createElement(bentoCardsData[5].icon, { 
+              className: `h-5 w-5 text-${bentoCardsData[5].gradient.split('-')[1]}-300` 
+            })}
             gradient={bentoCardsData[5].gradient}
             delay={bentoCardsData[5].delay}
             sizeClasses="col-span-1 row-span-1"
@@ -92,7 +104,9 @@ const BentoBox = () => {
           <BentoCard
             title={bentoCardsData[6].title}
             description={bentoCardsData[6].description}
-            icon={<bentoCardsData[6].icon className={`h-5 w-5 text-${bentoCardsData[6].gradient.split('-')[1]}-300`} />}
+            icon={React.createElement(bentoCardsData[6].icon, { 
+              className: `h-5 w-5 text-${bentoCardsData[6].gradient.split('-')[1]}-300` 
+            })}
             gradient={bentoCardsData[6].gradient}
             delay={bentoCardsData[6].delay}
             sizeClasses="col-span-1 md:col-span-3 row-span-1"
